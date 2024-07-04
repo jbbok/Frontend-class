63190732bc4422abbf722045bd26efa8489cc3e3
jbbok <qkrwjd94@gmail.com>
2024-07-04 16:30:41 +0900

    “240704”

--- 940430/240704 영화페이지/script.js
+++ 940430/240704 영화페이지/script.js
@@ -1,50 +1,57 @@
-// nav ul & li
+// top_nav
 const gnbLi = document.querySelectorAll(".topNav > li");
 gnbLi.forEach((li) => {
   li.addEventListener("mouseover", () => {
     const lnb = li.querySelector(".lnb");
     const aTag = li.querySelector("a");
     if (lnb) {
-      lnb.computedStyleMap.maxHeight = lnb.scrollHeight + "px";
-      lnb.computedStyleMap.opacity = "1";
+      lnb.style.maxHeight = lnb.scrollHeight + "px";
+      lnb.style.opacity = "1";
       aTag.classList.add("active");
     }
   });
-  li,
-    addEventListener("mouseout", () => {
-      const lnb = li.querySelector(".lnb");
-      const aTag = li.querySelector("a");
-      if (lnb) {
-        lnb.computedStyleMap.maxHeight = "0";
-        lnb.computedStyleMap.opacity = "0";
-        aTag.classList.remove("active");
-      }
-    });
-});
-
-// background image change
-
-const bgImg = document.querySelector("#bg_img");
-console.log(bgImg);
-const bgImgs = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg"];
-bgImg.style.backgroundImage = `url(./img/${bgImgs[0]})`;
-
-items.forEach(() => {
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    bgImgs.style.backgroundImage = `url(./img/${bgImgs[index]})`;
+  li.addEventListener("mouseout", () => {
+    const lnb = li.querySelector(".lnb");
+    const aTag = li.querySelector("a");
+    if (lnb) {
+      lnb.style.maxHeight = "0";
+      lnb.style.opacity = "0";
+      aTag.classList.remove("active");
+    }
   });
 });
 
-// card items
-const items = document.querySelectorAll("#card_items li");
+// background img
+const bgImgs = ["bg1.jpg"];
+const bgImg = document.querySelector("#bg_img");
+bgImg.style.backgroundImage = `url(./img/${bgImgs[0]})`;
 
-items.forEach(() => {
-  items.addEventListener("mouseover", () => {
-    items.style.transform = "translateY(-20px)";
-    items, (style.transition = "all 0.3s");
+// card item
+const items = document.querySelectorAll("#card_items li");
+items.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.style.transform = "translateY(-20px)";
+    item.style.transition = "all 0.3s";
   });
   item.addEventListener("mouseout", () => {
     item.style.transform = "translateY(0)";
+    item.style.background = "";
+  });
+});
+const pics = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg"];
+let i = 0;
+let slideInterval;
+let isTransitioning = false;
+const startSlideShow = () => {
+  slideInterval = setInterval(() => {
+    i = (i + 1) % pics.length;
+    updateSlide(i);
+  }, 4000);
+};
+
+items.forEach((item, index) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    bgImg.style.backgroundImage = `url(./img/${pics[index]})`;
   });
 });
