5ffc459f67ef164985883c9525e493244bfa76e7
jbbok <qkrwjd94@gmail.com>
2024-07-16 17:02:16 +0900

    “240716”

--- 240715/02 flex/script.js
+++ 240715/02 flex/script.js
@@ -1,3 +1,4 @@
+// Header Nav
 window.addEventListener("scroll", () => {
   // console.log("scroll");
   let scroll = window.scrollY;
@@ -9,4 +10,40 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("active");
   }
+});
+
+// Slick Slider
+$(".myslider").slick({
+  dots: false,
+  infinite: false,
+  autoplay: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+  ],
 });
