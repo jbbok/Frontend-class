7caff4738820a483a4937ef35ea5882a676dd75e
jbbok <qkrwjd94@gmail.com>
2024-10-18 16:39:15 +0900

    241018 node

---
+++ 241018/server.js
@@ -0,0 +1,16 @@
+// const { ApolloServer } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    text: String
+    film: String
+    allFilms: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
