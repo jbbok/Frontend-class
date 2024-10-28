bbcccbd4ceef59ee2cb4c931c755cb0fd48b7d1d
jbbok <qkrwjd94@gmail.com>
2024-10-28 21:39:40 +0900

    241028 ts

---
+++ 241028/02ts/src/Circle.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div<CircleProps>`
+  width: 200px;
+  height: 200px;
+  background: ${({ bgColor }) => bgColor};
+  border: 4px solid ${({ borderColor }) => borderColor};
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+const Circle = ({ bgColor, borderColor, text = "World!" }: CircleProps) => {
+  const [counter, setCounter] = useState<number | string>(1);
+  setCounter("1");
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {/* 단락회로평가 */}
+      {text} {counter}
+    </Container>
+  );
+};
+
+export default Circle;
